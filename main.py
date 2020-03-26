a47aad8a458d16e9d0b0c78259bb7531b2f52e2a
JossyrUpM <61509603+JossyrUpM@users.noreply.github.com>
2020-03-26 15:21:00 -0500

    Mi primera modificación

---
+++ main.py
@@ -0,0 +1,7 @@
+
+d = int(input('Dígite un numero '))
+b = int(input('Dígite un numero '))
+cuadrado=b**2
+raiz=d**(1/2)
+print("La raiz del primer numero es",raiz,)
+print("El cuadrado del segundo numero es",cuadrado,)
